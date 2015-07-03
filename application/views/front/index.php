92d82d92a90bdfdc3432f274e83392a6b8cf7238
Elias1bamboo <turrinelias@gmail.com>
2015-07-03 16:22:40 -0300

    Update del slider de la home por el nativo de BS

--- application/views/front/index.php
+++ application/views/front/index.php
@@ -1,7 +1,68 @@
 ﻿<? include(dirname(__FILE__)."/common/header.php") ?>
 
     <section id="slider">
-      <div class="container-fluid">
+      <div class="container-fluid" style="padding: 0;">
+        <div class="row">
+          <div class="col-xs-12">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+              <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+              <li data-target="#myCarousel" data-slide-to="1"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+              <div class="item active">
+                <img src="<?=base_url();?>assets_fe/img/slider1.jpg" alt="item1">
+                <div class="carousel-caption">
+                    <div class="container">
+                      <div class="row">
+                        <div class="col-xs-12">
+                          <h2>Coloración</h2>
+                          <h3>Productos de cuidado y tratamiento para el cabello.</h3>
+                          <p>Issue brinda los mejores productos para obtener el mejor cuidado para tu cabello.</p>
+                          <a href="#">ver más</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+
+              <div class="item">
+                <img src="<?=base_url();?>assets_fe/img/slider1.jpg" alt="item2">
+                <div class="carousel-caption">
+                    <div class="container">
+                      <div class="row">
+                        <div class="col-xs-12">
+                          <h2>Coloración</h2>
+                          <h3>Productos de cuidado y tratamiento para el cabello.</h3>
+                          <p>Issue brinda los mejores productos para obtener el mejor cuidado para tu cabello.</p>
+                          <a href="#">ver más</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+
+            </div>
+
+            <!-- Left and right controls -->
+              <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div> 
+
+
+      <!-- <div class="container">
         <div class="row">
           <div class="col-xs-12">
             <ul class="bxslider">
@@ -28,7 +89,7 @@
             </ul>
           </div>
         </div>    
-      </div>
+      </div> -->
     </section>
     <section id="nuestros-productos">
       <h3>nuestros productos</h3>
